a924049e3e82a227f3acbb7656174f94692a3845
ChaitanyaRajNimap <chaitanyarajiwade@nimapinfotech.com>
2023-02-17 10:09:33 +0530

    Merge sort implemented

--- temp.js
+++ temp.js
@@ -1,15 +1,19 @@
-const insertionSort = (arr) => {
-  for (let i = 1; i < arr.length; i++) {
-    let numToInsrt = arr[i];
-    let j = i - 1;
-    while (j >= 0 && arr[j] > numToInsrt) {
-      arr[j + 1] = arr[j];
-      j -= 1;
-    }
-    arr[j + 1] = numToInsrt;
+const quickSort = (arr) => {
+  if (arr.length < 2) {
+    return arr;
   }
+  let pivot = arr[arr.length - 1];
+  let left = [];
+  let right = [];
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i] < pivot) {
+      left.push(arr[i]);
+    } else {
+      right.push(arr[i]);
+    }
+  }
+  return [...quickSort(left), pivot, ...quickSort(right)];
 };
 
-const arr1 = [-63, -9, 55, -74, 2];
-insertionSort(arr1);
-console.log(arr1);
+const arr1 = [-200, 100, -63, 120, -9, 55, -74, 2, -90];
+console.log(quickSort(arr1));
